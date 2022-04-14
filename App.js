36f1bacb3a1f24c68caafd5611f40f95570ca8a7
yeffry <yeffry45@gmail.com>
2022-04-14 13:58:15 -0400

    add select filter component and handling of option values

--- App.js
+++ App.js
@@ -1,30 +1,38 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import moon from './images/icon-moon.svg'
 import sun from './images/icon-sun.svg'
 import '/index.css'
 
 const App = () => {
   const [theme, setTheme] = useState('light')
+  const [filter, setFilter] = useState('')
+  
+  const regions = ["Africa", "Americas", "Asia", "Europe", "Oceania"]
 
   const handleTheme = () => {
     const toggle = theme === 'light' ? 'dark' : 'light'
     document.body.setAttribute('data-theme', toggle)
     setTheme(toggle)
   }
+
+  const handleSetFilter = (e) => {
+    setFilter(e.target.value)
+  }
   return (
     <div className="app">
-        <header className='header__container'>
-          <h1>Where in the world?</h1>
+      <header className='header__container'>
+        <h1>Where in the world?</h1>
 
-          <div className='header__theme'>
-            <button onClick={handleTheme}>
-              <img src={theme === 'light' ? moon : sun} alt="theme icon" />
-            </button>
-              
-            <span>Dark Mode</span>
-          </div>
-        </header>
+        <div className='header__theme'>
+          <button onClick={handleTheme}>
+            <img src={theme === 'light' ? moon : sun} alt="theme icon" />
+          </button>
+            
+          <span>Dark Mode</span>
+        </div>
+      </header>
 
+      <div className="filter__container">
         <form className='form__container'>
           <label htmlFor="country">
             <input 
@@ -35,6 +43,16 @@ const App = () => {
             />
           </label>
         </form>
+
+        <select value={filter} name="region" id="region" class="" onChange={handleSetFilter}>
+          <option value="" hidden>Filter by Region</option>
+          {regions.map((region) => {
+            return (
+              <option key={region} value={region}>{region}</option>
+            )
+          })}
+        </select>
+      </div>
     </div>
     
   )
