77f3946b47503a2fbb1096c8d0b506203d92d84b
yeffry <yeffry45@gmail.com>
2022-04-23 10:31:12 -0400

    add content wrapper for better desktop media query handling

--- Country.js
+++ Country.js
@@ -54,36 +54,41 @@ const Country = () => {
             <div className="country__data">
               <h2>{name}</h2>
 
-              <div className="country__data-left">
-                <h3 className="text-dark">Native name: <span className="text-light">{nativeName}</span></h3>
-                <h3 className="text-dark">Population: <span className="text-light">{population}</span></h3>
-                <h3 className="text-dark">Region: <span className="text-light">{region}</span></h3>
-                <h3 className="text-dark">Sub Region: <span className="text-light">{subregion}</span></h3>
-                <h3 className="text-dark">Capital: <span className="text-light">{capital}</span></h3>
-              </div>
+              <div className="country__data-wrapper">
+                <div className="country__data-left">
+                  <h3 className="text-dark">Native name: <span className="text-light">{nativeName}</span></h3>
+                  <h3 className="text-dark">Population: <span className="text-light">{population}</span></h3>
+                  <h3 className="text-dark">Region: <span className="text-light">{region}</span></h3>
+                  <h3 className="text-dark">Sub Region: <span className="text-light">{subregion}</span></h3>
+                  <h3 className="text-dark">Capital: <span className="text-light">{capital}</span></h3>
+                </div>
 
-              <div className="country__data-right">
-                <h3 className="text-dark">Top Level Domain: <span className="text-light">{topLevelDomain[0]}</span></h3>
-                <h3 className="text-dark">Currencies: <span className="text-light">{currencies[0].name}</span></h3>
-                <h3 className="text-dark">Languages:  
-                  {languages.map((language, index) => 
-                    <span key={language.iso639_1} className="text-light"> {(index ? ', ' : '') + language.name} </span>)
-                  }
-                </h3>
+                <div className="country__data-right">
+                  <h3 className="text-dark">Top Level Domain: <span className="text-light">{topLevelDomain[0]}</span></h3>
+                  <h3 className="text-dark">Currencies: <span className="text-light">{currencies[0].name}</span></h3>
+                  <h3 className="text-dark">Languages:  
+                    {languages.map((language, index) => 
+                      <span key={language.iso639_1} className="text-light"> {(index ? ', ' : '') + language.name} </span>)
+                    }
+                  </h3>
+                </div>
               </div>
 
               <div className="country__data-borders">
 
-                <h3>Border Countries:</h3>
+                
                 {borders ? borders.map((border) => {
                   return (
-                    <button
-                      className="country__border-btn" 
-                      onClick={() => fetchBorderCountry(border)} 
-                      key={border}
-                    >
-                    {border}
-                    </button>
+                    <div>
+                      <h3>Border Countries:</h3>
+                      <button
+                        className="country__border-btn" 
+                        onClick={() => fetchBorderCountry(border)} 
+                        key={border}
+                      >
+                      {border}
+                      </button>
+                    </div>
                   )
                 }): null}
               </div>
